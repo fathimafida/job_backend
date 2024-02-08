cd345cae925b1fbdcff9686a3b3cb5f4a0f44a50
Fathima Fida <fidafirozkattungal@gmail.com>
2024-02-08 18:12:53 +0530

    add search filed

--- jobs/views.py
+++ jobs/views.py
@@ -1,9 +1,13 @@
 from jobs.models import PostJob
 from jobs.serializers import PostJobSerializer
 from rest_framework import generics
+from rest_framework.filters import SearchFilter, OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class PostJobListAPIView(generics.ListCreateAPIView):
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    search_fields = ["title", "place", "companyName"]
     queryset = PostJob.objects.all()
     serializer_class = PostJobSerializer
 
@@ -11,4 +15,3 @@ class PostJobListAPIView(generics.ListCreateAPIView):
 class PostJobDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = PostJob.objects.all()
     serializer_class = PostJobSerializer
-    
