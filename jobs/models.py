0bf61dcb4c4bb92a71a9e3742b462cbb253473a8
Fathima Fida <fidafirozkattungal@gmail.com>
2024-02-09 22:22:35 +0530

    createdAt  and updatedAt added in models

--- jobs/models.py
+++ jobs/models.py
@@ -9,6 +9,8 @@ class PostJob(models.Model):
     description = models.TextField()
     jobType = models.CharField(max_length=255)
     url = models.URLField(max_length=1500)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
